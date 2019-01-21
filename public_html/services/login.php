f9b641cb95c56c1bf32d0bde23b6bae55026125b
Karlo Njegovan <karlo.njegovan@gmail.com>
2019-01-22 00:22:40 +0100

    Inital push with class for database connection and basic login service

---
+++ public_html/services/login.php
@@ -0,0 +1,30 @@
+<?php
+
+
+/**
+ * Created by PhpStorm.
+ * User: Forgift
+ * Date: 08/01/2019
+ * Time: 16:18
+ */
+
+include "Database.php";
+
+$db = new Database();
+
+$username = $db->escapeString($_GET["user"]);
+$pass = $db->escapeString($_GET["pass"]);
+
+$result = $db ->executeQuery("SELECT * FROM `mjerenje_testAplikacija`.`Korisnik` WHERE kor_ime = '$username' and lozinka = '$pass'");
+$db->close();
+
+if(mysqli_num_rows($result)>0){
+  echo  "Uspjesno logiranje";
+}
+else{
+    echo "Neuspjeh";
+}
+
+
+
+?>
