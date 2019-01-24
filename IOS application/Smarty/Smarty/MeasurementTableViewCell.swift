a6f09286832e3b48a1e0f1eaeb8fb4e6fb40b3a6
Matija <ljescanin@gmail.com>
2019-01-24 23:49:26 +0100

    Klasa mjerenje
    Dodana klasa za mjerenja te kontroleri za pocetnu stranicu

---
+++ IOS application/Smarty/Smarty/MeasurementTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MeasurementTableViewCell.swift
+//  Smarty
+//
+//  Created by Matija on 24/01/2019.
+//  Copyright © 2019 Matija. All rights reserved.
+//
+
+import UIKit
+
+class MeasurementTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
